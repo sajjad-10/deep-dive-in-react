733d0ec4f894b59a3f1240cb985cd8be6e39424d
sajjad-10 <sajjadfallahi.official@gmail.com>
2022-05-06 17:00:44 +0430

    add btn prop Types

--- simple-task-tracker/src/components/Button.js
+++ simple-task-tracker/src/components/Button.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import PropTypes from "prop-types";
+const Button = ({ color, text }) => {
+    return (
+        <button style={{ backgroundColor: color }} className="btn">
+            {text}
+        </button>
+    );
+};
 
-const Button = ({color,text}) => {
-  return <button style={{backgroundColor: color}} className="btn">{text}</button>
-}
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+};
 
-export default Button
+export default Button;
