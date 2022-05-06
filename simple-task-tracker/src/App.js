1be116d8f946ab42be764531d2bcae3be14fb888
sajjad-10 <sajjadfallahi.official@gmail.com>
2022-05-06 14:20:43 +0430

    intro header componen

--- simple-task-tracker/src/App.js
+++ simple-task-tracker/src/App.js
@@ -1,7 +1,8 @@
+import Header from "./components/Header";
 function App() {
     return (
         <div className="container">
-            <h1>Hellow From React</h1>
+            <Header/>
         </div>
     );
 }
---
+++ simple-task-tracker/src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+        <h1>Task Trackerrot</h1>
+    </header>
+  )
+}
+
+export default Header
