1d23c24ac2a5e5aac9c8a4e895ac1143875331d1
sajjad-10 <sajjadfallahi.official@gmail.com>
2022-05-06 20:24:52 +0430

    -and set what happen if empty

--- simple-task-tracker/src/App.js
+++ simple-task-tracker/src/App.js
@@ -24,7 +24,11 @@ function App() {
     return (
         <div className="container">
             <Header />
-            <Tasks tasks={tasks} onDelete={deleteTask} />
+            {tasks.length > 0 ? (
+                <Tasks tasks={tasks} onDelete={deleteTask} />
+            ) : (
+                "No tasks to Show"
+            )}
         </div>
     );
 }
