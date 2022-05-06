6fe545518ac05549d33bc846cc675b503c7095fa
sajjad-10 <sajjadfallahi.official@gmail.com>
2022-05-06 21:20:45 +0430

    create Add Task component

--- simple-task-tracker/src/App.js
+++ simple-task-tracker/src/App.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+
 function App() {
     const [tasks, setTasks] = useState([
         {
@@ -32,6 +34,7 @@ function App() {
     return (
         <div className="container">
             <Header />
+            <AddTask />
             {tasks.length > 0 ? (
                 <Tasks
                     tasks={tasks}
---
+++ simple-task-tracker/src/components/AddTask.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+const AddTask = () => {
+    return (
+        <form className="add-form">
+            <div className="form-control">
+                <label>Task</label>
+                <input type="text" placeholder="Add Task" />
+            </div>
+            <div className="form-control">
+                <label>Day & Time</label>
+                <input type="text" placeholder="Add Task" />
+            </div>{" "}
+            <div className="form-control form-control-check">
+                <label>Set Reminder</label>
+                <input type="checkbox" />
+            </div>
+            <input className="btn btn-block" type="submit" value="Save Task" />
+        </form>
+    );
+};
+
+export default AddTask;
