9742ec327b6f7dff6c4e17d998b1082f67544cdb
sajjad-10 <sajjadfallahi.official@gmail.com>
2022-05-06 17:11:40 +0430

    setup event for button

--- simple-task-tracker/src/components/Header.js
+++ simple-task-tracker/src/components/Header.js
@@ -2,11 +2,14 @@ import PropTypes from "prop-types";
 import Button from "./Button";
 
 const Header = ({ title }) => {
+    const onClick = () => {
+        console.log('Click');
+    }
 
     return (
         <header className="header">
             <h1>{title}</h1>
-            <Button color='green' text='Hello'/>
+            <Button onClick={onClick} color='green' text='Hello' />
         </header>
     );
 };
