96d17060f079f440a05fbf2802227cc5b7fdde4f
sajjad-10 <sajjadfallahi.official@gmail.com>
2022-05-07 16:02:15 +0430

    handle add new task

--- simple-task-tracker/src/App.js
+++ simple-task-tracker/src/App.js
@@ -5,6 +5,7 @@ import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 function App() {
+    const[showAddTask, setShowAddTask]=useState(false)
     const [tasks, setTasks] = useState([
         {
             id: 1,
@@ -40,7 +41,7 @@ function App() {
     return (
         <div className="container">
             <Header />
-            <AddTask onAdd={addTask} />
+            {showAddTask && <AddTask onAdd={addTask} />}
             {tasks.length > 0 ? (
                 <Tasks
                     tasks={tasks}
