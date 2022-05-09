ec6e179d9da5db8500167e07392f7b8ae7e1df40
sajjad-10 <sajjadfallahi.official@gmail.com>
2022-05-09 20:56:48 +0430

    -add states on compo AddTransaction

--- expense-tracker-app/src/components/AddTransaction.js
+++ expense-tracker-app/src/components/AddTransaction.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const AddTransaction = () => {
+    const [text, setText] = useState("");
+    const [amount, setAmount] = useState(0);
     return (
         <>
             {" "}
@@ -8,14 +10,24 @@ export const AddTransaction = () => {
             <form>
                 <div className="form-control">
                     <label htmlFor="text">Text</label>
-                    <input type="text" placeholder="Enter text..." />
+                    <input
+                        value={text}
+                        onChange={(e) => setText(e.target.value)}
+                        type="text"
+                        placeholder="Enter text..."
+                    />
                 </div>
                 <div className="form-control">
                     <label htmlFor="amount">
                         Amount <br />
                         (negative - expense, positive - income)
                     </label>
-                    <input type="number" placeholder="Enter amount..." />
+                    <input
+                        value={amount}
+                        onChange={(e) => setAmount(e.target.value)}
+                        type="number"
+                        placeholder="Enter amount..."
+                    />
                 </div>
                 <button className="btn">Add transaction</button>
             </form>
