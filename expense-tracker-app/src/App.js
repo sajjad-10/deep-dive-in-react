ba0f5103b08e9ca63bfddef72beea5b92a261cde
sajjad-10 <sajjadfallahi.official@gmail.com>
2022-05-09 20:44:50 +0430

    generate All component

--- expense-tracker-app/src/App.js
+++ expense-tracker-app/src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
 import { Header } from "./components/Header";
 import { Balance } from "./components/Balance";
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
+import { AddTransaction } from "./components/AddTransaction";
 
 
 function App() {
@@ -8,7 +11,10 @@ function App() {
         <div>
             <Header />
             <div className="container">
-              <Balance />
+                <Balance />
+                <IncomeExpenses />
+                <TransactionList />
+                <AddTransaction />
             </div>
         </div>
     );
