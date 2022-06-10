3a441c0adc35fb59d59f90a07c75be230e841a5e
sajjad-10 <sajjadfallahi.official@gmail.com>
2022-06-10 23:50:50 +0430

    work on request

--- image-gallery/src/App.js
+++ image-gallery/src/App.js
@@ -3,7 +3,15 @@ import React, { useState, useEffect } from "react";
 function App() {
     const [images, setImages] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
-    const [term, setTerm] = useState("[]");
+    const [term, setTerm] = useState("");
+    useEffect(() => {
+        fetch(
+            `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+        )
+            .then((res) => res.json())
+            .then((data) => console.log(data))
+            .catch((err) => console.log(err));
+    },[]);
     return <div className="container mx-auto"></div>;
 }
 
