18fdb3368bcc20cce0a4c5fce014d17b6907f959
sajjad-10 <sajjadfallahi.official@gmail.com>
2022-06-11 20:06:42 +0430

    add component imageCard

--- image-gallery/src/App.js
+++ image-gallery/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import ImageCard from "./components/ImageCard";
 
 function App() {
     const [images, setImages] = useState([]);
@@ -9,10 +10,21 @@ function App() {
             `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
         )
             .then((res) => res.json())
-            .then((data) => console.log(data))
+            .then((data) => {
+                setImages(data.hits);
+                setIsLoading(false);
+            })
             .catch((err) => console.log(err));
-    },[]);
-    return <div className="container mx-auto"></div>;
+    }, []);
+    return (
+        <div className="container mx-auto">
+            <div className="grid grid-cols-3 gap-4">
+                {images.map((image) => (
+                    <ImageCard key={image.id} image={image}/>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
