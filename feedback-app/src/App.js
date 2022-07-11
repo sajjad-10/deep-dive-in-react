9337c6bda6b4dbad8d38af9b129b2ffa21a1a83c
sajjad-10 <sajjadfallahi.official@gmail.com>
2022-07-11 23:07:20 +0430

    render Feedback Item as list

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from './data/FeedbackData'
 
 function App() {
+  const [ feedback,setFeedback] =useState(FeedbackData)
     return (
         <div className="container">
             <Header />
+            <FeedbackList feedback={feedback} />
         </div>
     );
 }
