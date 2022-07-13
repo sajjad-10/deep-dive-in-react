ffa7781bb4ca512c72fdb086a5c8496fb6f00e96
sajjad-10 <sajjadfallahi.official@gmail.com>
2022-07-13 18:45:24 +0430

    component for form input FeedbackForm

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from './data/FeedbackData'
 import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
@@ -10,11 +11,15 @@ function App() {
         if (window.confirm('Are you sure, you want to delete?')) { setFeedback(feedback.filter((item) => item.id !== id)) }
     }
     return (
-        <div className="container">
+        <>
             <Header />
-            <FeedbackStats feedback={feedback}/>
-            <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-        </div>
+            <div className="container">
+                <FeedbackForm />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+            </div>
+        </>
+
     );
 }
 
