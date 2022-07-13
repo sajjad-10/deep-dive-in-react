de387280fbb32ac2a13702391f500a3d3f4e1269
sajjad-10 <sajjadfallahi.official@gmail.com>
2022-07-13 21:47:15 +0430

    add animate Us framer-motion

--- feedback-app/src/components/FeedbackList.jsx
+++ feedback-app/src/components/FeedbackList.jsx
@@ -1,14 +1,23 @@
+import { motion, AnimatePresence } from "framer-motion";
 import FeedbackItem from "./FeedbackItem";
-function FeedbackList({ feedback,handleDelete }) {
+function FeedbackList({ feedback, handleDelete }) {
     if (!feedback || feedback.length === 0) {
         return <p>No Feedback Yet</p>;
     }
 
     return (
         <div className="feedback-list">
-            {feedback.map((item) => (
-                <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
-            ))}
+            <AnimatePresence>
+                {feedback.map((item) => (
+                    <motion.dev key={item.id} initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
+                        <FeedbackItem
+                            key={item.id}
+                            item={item}
+                            handleDelete={handleDelete}
+                        />
+                    </motion.dev>
+                ))}
+            </AnimatePresence>
         </div>
     );
 }
