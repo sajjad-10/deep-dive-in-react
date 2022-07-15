1dc82f30fb99e1e33c4fa6e96b2b519cc5a4bb00
sajjad-10 <sajjadfallahi.official@gmail.com>
2022-07-15 11:16:04 +0430

    Edit Feedback Event

--- feedback-app/src/context/FeedbackContext.jsx
+++ feedback-app/src/context/FeedbackContext.jsx
@@ -14,24 +14,32 @@ export const FeedbackProvider = ({ children }) => {
             id: 2,
             text: "This is from Context 2",
             rating: 5,
-        },        {
+        },
+        {
             id: 3,
             text: "This is from Context 3",
             rating: 9,
         },
     ]);
+    const [feedbackEdit, setFeedbackEdit] = useState({ item: {}, edit: false });
+    // Add Feedback
     const addFeedback = (newFeedback) => {
         newFeedback.id = uuidv4();
         setFeedback([newFeedback, ...feedback]);
     };
+    // Delete Feedback
     const deleteFeedback = (id) => {
         if (window.confirm("Are you sure, you want to delete?")) {
             setFeedback(feedback.filter((item) => item.id !== id));
         }
     };
+    // Set item to be unbated
+    const editFeedback = (item) => {
+        setFeedbackEdit({ item, edit: true });
+    };
     return (
         <FeedbackContext.Provider
-            value={{ feedback, deleteFeedback, addFeedback }}
+            value={{ feedback, deleteFeedback, addFeedback, editFeedback }}
         >
             {children}
         </FeedbackContext.Provider>
