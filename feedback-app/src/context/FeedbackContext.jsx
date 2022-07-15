86ab932bb17ee486c6ba05e2b9231420d9412d76
sajjad-10 <sajjadfallahi.official@gmail.com>
2022-07-15 18:08:29 +0430

    set Proxy / add Feedback on db

--- feedback-app/src/context/FeedbackContext.jsx
+++ feedback-app/src/context/FeedbackContext.jsx
@@ -1,4 +1,3 @@
-import { v4 as uuidv4 } from "uuid";
 import { createContext, useState, useEffect } from "react";
 
 const FeedbackContext = createContext();
@@ -12,17 +11,23 @@ export const FeedbackProvider = ({ children }) => {
     }, []);
     //Fetch feedback
     const fetchFeedback = async () => {
-        const response = await fetch(
-            `http://localhost:5000/feedback?_sort=id&_order=desc`
-        );
+        const response = await fetch(`/feedback?_sort=id&_order=desc`);
+
         const data = await response.json();
         setFeedback(data);
         setIsLoading(false);
     };
     // Add Feedback
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4();
-        setFeedback([newFeedback, ...feedback]);
+    const addFeedback = async (newFeedback) => {
+        const response = await fetch("/feedback", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newFeedback),
+        });
+        const data = await response.json();
+        setFeedback([data, ...feedback]);
     };
     // Delete Feedback
     const deleteFeedback = (id) => {
