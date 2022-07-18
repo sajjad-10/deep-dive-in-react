94bc069d36282896b86a633adafb8ed38743ba48
sajjad-10 <sajjadfallahi.official@gmail.com>
2022-07-18 14:23:42 +0430

    Setup githuub Context

--- github-user-finder/src/App.js
+++ github-user-finder/src/App.js
@@ -1,26 +1,30 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Footer from './components/layout/Footer';
-import Navbar from './components/layout/Navbar';
-import About from './components/pages/About';
-import Home from './components/pages/Home';
-import NotFound from './components/pages/NotFound';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Footer from "./components/layout/Footer";
+import Navbar from "./components/layout/Navbar";
+import About from "./components/pages/About";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
+
+import { GithubProvider } from "./context/github/GithubContext";
 function App() {
-  return (
-    <Router >
-      <div className='flex flex-col justify-between h-screen'>
-        <Navbar />
-        <main className='container mx-auto px-3 pb-12'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/notfound' element={<NotFound />} />
-            <Route path='/*' element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
-  );
+    return (
+        <GithubProvider>
+            <Router>
+                <div className="flex flex-col justify-between h-screen">
+                    <Navbar />
+                    <main className="container mx-auto px-3 pb-12">
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/about" element={<About />} />
+                            <Route path="/notfound" element={<NotFound />} />
+                            <Route path="/*" element={<NotFound />} />
+                        </Routes>
+                    </main>
+                    <Footer />
+                </div>
+            </Router>
+        </GithubProvider>
+    );
 }
 
 export default App;
--- github-user-finder/src/components/users/Users.jsx
+++ github-user-finder/src/components/users/Users.jsx
@@ -1,32 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect, useContext } from "react";
 import Spinner from "../layout/Spinner";
 import UserItem from "./UserItem";
+import GithubContext from "../../context/github/GithubContext";
 function Users() {
-    const [users, setUsers] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const githubContext = useContext(GithubContext);
+    const { loading, users, fetchUsers } = githubContext;
+
     useEffect(() => {
         fetchUsers();
     }, []);
-    const fetchUsers = async () => {
-        const response = await fetch(
-            `${process.env.REACT_APP_GITHUB_URL}/users`,
-            {
-                headers: {
-                    Authorization: `token ${process.env.React_APP_GITHUB_TOKEN}`,
-                },
-            }
-        );
-        const data = await response.json();
-        setUsers(data);
-        setLoading(false);
-    };
     if (loading) {
         return <Spinner />;
     }
     return (
         <div className="grid grid-col-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
             {users.map((user) => (
-                <UserItem user={user} key={user.id}/>
+                <UserItem user={user} key={user.id} />
             ))}
         </div>
     );
